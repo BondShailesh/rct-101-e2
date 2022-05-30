298d6c89d6f42b79cd4aa79a7943394a27959725
BondShailesh <you@example.comShaileshrajvicky@gmail.com>
2022-05-30 16:26:47 +0530

    counld Not Solve

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,23 +1,17 @@
 import React from "react";
-
-const Product = () => {
-  // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+import {Text,Image,Box,Stack,Heading,Tag,TagLabel}  from "@chakra-ui/react";
+const Product = ({dat}) => {
+  console.log(dat);
   return (
     <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
+      {/* <Image data-cy="product-image" src={dat.imageSrc} gwtar/>
+      <Text data-cy="product-category">{dat.category} etw </Text>
       <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
+        <TagLabel data-cy="product-gender">{dat.gender} ewt </TagLabel>
       </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
+      <Heading data-cy="product-title">{dat.title} ewt</Heading>
+      <Box data-cy="product-price">{dat.price} ewtt</Box> */}
+      <h1>beatgoiuw</h1>
     </Stack>
   );
 };
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,42 @@
 import React from "react";
-
+import {Flex,Grid, useStyleConfig}  from "@chakra-ui/react";
+import AddProduct from "./AddProduct"
+import { useState } from "react";
+import { useEffect } from "react";
+import axios from "axios"
+import Product from "./Product";
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
+  // const Flex = () => <div />;
+  // const Grid = () => <div />;
 
+const [data1,setDta1] = useState([]);
+const [per,setPer] = useState();
+
+ useEffect(()=>{
+   const data = async ()=>{
+    const res = await axios("http://localhost:8080/products");
+    setDta1([...data1,res.data])
+    // setPer(res.data[0].id)
+   }
+   data()
+ },[])
+console.log(data1);
   return (
     <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
+      {/* <AddProduct/> */}
+
+      <Grid>
+       {/* {data1.map((dat0)=>( */}
+         <Product/>
+      {/* //  ))} */}
+        </Grid>
+
+
       {/* Pagination */}
+
     </Flex>
+  
   );
 };
 
